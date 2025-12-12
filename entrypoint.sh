f243b9e550c9d467937cefee27d9e753fed5b971
magicstone1412 <nguyenhahuan@gmail.com>
2025-12-12 10:34:58 +0700

    add sleep 3s for hsd running

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ set -eu
 echo "[INFO] Entrypoint starting..."
 echo "[INFO] LOOP_SECONDS=${LOOP_SECONDS:-3600}"
 
+echo "[INFO] Sleeping for 3 seconds for hsd running ..."
+sleep 3
 # Start teleshake loop script
 exec /app/scripts/teleshake.sh
